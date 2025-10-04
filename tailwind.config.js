c29a47094ef786a8aced51eff9285927e574681f
JorgeTabuada <211917068+JorgeTabuada@users.noreply.github.com>
2025-10-03 22:10:23 -0400

    Corrigir configurações do Tailwind CSS para deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -61,5 +59,3 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
-export default config
